2047d0fde925cd01e87abe12d8c662183c936a03
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-11-13 16:42:02 -0700

    Stub Boot project with JPA project dependency

---
+++ DogTracker/src/main/java/com/skilldistillery/dogs/DogTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogTrackerApplication.class, args);
+	}
+
+}
---
+++ DogTracker/src/test/java/com/skilldistillery/dogs/DogTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
