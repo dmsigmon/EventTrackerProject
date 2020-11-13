0a08c4726564750de0c36079faa4b15c3a0b45e9
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-11-13 16:11:15 -0700

    validate JPA/Hibernate config

--- JPADog/src/main/java/com/skilldistillery/dogs/entites/Dog.java
+++ JPADog/src/main/java/com/skilldistillery/dogs/entites/Dog.java
@@ -1,12 +1,69 @@
 package com.skilldistillery.dogs.entites;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Dog {
-	
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	private String name;
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Dog [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dog other = (Dog) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public Dog() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- JPADog/src/test/java/com/skilldistillery/dogs/entites/DogTest.java
+++ JPADog/src/test/java/com/skilldistillery/dogs/entites/DogTest.java
@@ -2,33 +2,50 @@ package com.skilldistillery.dogs.entites;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class DogTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Dog dog;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("DogPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		dog = em.find(Dog.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		dog = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("test dog entity mapping")
+	void test1() {
+		assertNotNull(dog);
+		assertEquals("Coda", dog.getName());
 	}
 
 }
